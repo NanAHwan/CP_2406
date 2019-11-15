7ea5c94ebfff15fd7496b44790b4fba4954b7b48
NanAHwan <a.nan@my.jcu.edu.au>
2019-11-15 12:38:01 +0800

    Create a MovieInfo class

---
+++ CodingExcercises/MovieInfo.java
@@ -0,0 +1,12 @@
+public class MovieInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Favorite movie quote: They are not infinite.");
+        System.out.println("Movie Name: The legend of 1900");
+        System.out.println("The Character: 1900");
+        System.out.println("Year of movie: 1998");
+
+    }
+
+}
